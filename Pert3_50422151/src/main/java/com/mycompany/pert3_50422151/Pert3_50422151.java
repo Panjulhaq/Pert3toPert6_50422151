cdef5fde5f41919fb8da6ea5cfcc582bf31a13a8
Panjulhaq <haqonijul24@gmail.com>
2025-11-29 11:33:00 +0700

    first commit

---
+++ Pert3_50422151/src/main/java/com/mycompany/pert3_50422151/Pert3_50422151.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3_50422151;
+
+/**
+ *
+ * @author Amay
+ */
+public class Pert3_50422151 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Pert6_50422151/src/main/java/com/mycompany/pert6_50422151/repository/MahasiswaRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert6_50422151.repository;
+
+import com.mycompany.pert6_50422151.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Amay
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Integer> {
+    
+
+}
